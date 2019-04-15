df102af2f3c2c607fb1110b284bad6fc113995e7
e <791758820@qq.com>
2019-04-15 11:45:05 +0800

    config-index.js

---
+++ config/index.js
@@ -0,0 +1,13 @@
+export default {
+  /**
+   * @description token在Cookie中存储的天数，默认1天
+   */
+  cookieExpires: 1,
+  /**
+   * @description api请求基础路径
+   */
+  baseUrl: {
+    dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
+    pro: 'https://produce.com'
+  }
+}
